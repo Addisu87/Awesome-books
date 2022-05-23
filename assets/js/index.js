61979c426d62df3e294142d7d93d9975121cdec1
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-05-23 23:49:01 +0300

    Added the remove btn functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,27 +6,40 @@ const bookForm = document.getElementsByTagName('form')[0];
 
 const addBtn = document.getElementById('add-btn');
 
+const removeBtn = document.getElementsByClassName('remove-btn');
+
 let booksArr = [];
 
 bookForm.addEventListener('submit', (event) => {
-    event.preventDefault();
+  event.preventDefault();
 });
 
-function AddBook(title = titleBookInput, author = authorBookInput) {
-    booksArr.push({title: title.value, author: author.value});
+function removeBook() {
+  booksArr = booksArr.filter(
+    (book) => book.title !== this.parentElement.children[0].innerHTML
+  );
+  console.log(booksArr);
+}
 
-    const bookElement = `
+function AddBook(title = titleBookInput, author = authorBookInput) {
+  booksArr.push({ title: title.value, author: author.value });
+
+  const bookElement = `
     <div class="book">
         <p class="book-title">${title.value}</p>
         <p class="book-author">${author.value}</p>
         <button class="remove-btn">Remove</button>
     </div>`;
 
-    bookList.insertAdjacentHTML("beforeend", bookElement);
+  bookList.insertAdjacentHTML('beforeend', bookElement);
+
+  removeBtn.addEventListener('click', () => {
+    removeBtn();
+  });
 
     console.log(booksArr);
 }
 
 addBtn.addEventListener('click', () => {
-    AddBook();
+  AddBook();
 });
