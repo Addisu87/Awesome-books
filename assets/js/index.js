cc248f0d24250c37b852802b28e54b9c8ef26c78
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-05-30 09:54:07 +0300

    Modify errors in css file

--- assets/js/index.js
+++ assets/js/index.js
@@ -41,7 +41,7 @@ class Book {
   // Remove a Book
   static remove() {
     Book.booksArr = Book.booksArr.filter(
-      (book) => +book.id !== +this.parentElement.id
+      (book) => +book.id !== +this.parentElement.id,
     );
     this.parentElement.remove();
 
@@ -102,16 +102,16 @@ class Book {
 function showThisSection(link) {
   links.forEach((lk) => {
     if (
-      link.getAttribute('href') === lk.getAttribute('href') &&
-      !link.classList.contains('highlight-link')
+      link.getAttribute('href') === lk.getAttribute('href')
+      && !link.classList.contains('highlight-link')
     ) {
       document
         .getElementById(lk.getAttribute('href').replace('#', ''))
         .classList.toggle('show-section');
       lk.classList.toggle('highlight-link');
     } else if (
-      link.getAttribute('href') !== lk.getAttribute('href') &&
-      lk.classList.contains('highlight-link')
+      link.getAttribute('href') !== lk.getAttribute('href')
+      && lk.classList.contains('highlight-link')
     ) {
       document
         .getElementById(lk.getAttribute('href').replace('#', ''))
@@ -140,7 +140,7 @@ addBtn.addEventListener('click', () => {
     const newBook = new Book(
       Book.booksArr.length,
       titleBookInput.value,
-      authorBookInput.value
+      authorBookInput.value,
     );
     newBook.Add();
     showCounter();
